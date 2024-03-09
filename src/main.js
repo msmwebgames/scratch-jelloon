52e941ef01637c70adbbb32a62ad3e98cc80a857
Matthew Sant-Miller <msantmiller@gmail.com>
2024-03-09 18:01:43 -0500

    Add initial game files

---
+++ src/main.js
@@ -0,0 +1,28 @@
+const rubTarget = 5;
+let rubCount = 0;
+
+const boardElement = document.querySelector('#board');
+
+const jelloonElement = document.querySelector('#image');
+jelloonElement.addEventListener('click', handleJelloonClick);
+
+const messageElement = document.querySelector('#message');
+messageElement.innerText = 'Rub count: ' + rubCount;
+
+function addRub() {
+    rubCount++;
+}
+
+function handleJelloonClick(event) {
+    addRub();
+    messageElement.innerText = 'Rub count: ' + rubCount;
+
+    if (rubCount >= rubTarget) {
+        const jelloonElement = document.querySelector('#image');
+        jelloonElement.setAttribute('src', 'res/images/happyjelloon.png');
+        jelloonElement.classList.replace('jelloon', 'happyjelloon');
+
+        messageElement.innerText = 'Jelloon says thunks :]';
+
+    }
+}
